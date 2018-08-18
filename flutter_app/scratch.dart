67e19c74529c49ae66fef860e5c7ab10d3aec48b
tanglie <tanglie23@163.com>
2018-08-18 20:21:12 +0800

    hello world

---
+++ flutter_app/scratch.dart
@@ -0,0 +1,9 @@
+printInteger(int aNumber) {
+  print('The number is $aNumber.'); // Print to console.
+}
+
+// This is where the app starts executing.
+main() {
+  var number = 42; // Declare and initialize a variable.
+  printInteger(number); // Call a function.
+}
